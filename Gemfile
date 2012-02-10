99f32a083bf4845b5ca71de172f92bcf6a7214c2
Alokesh Das <alokesh@gmail.com>
2012-02-10 21:37:02 +0530

    Make a basic User model (including secure passwords)

--- Gemfile
+++ Gemfile
@@ -8,6 +8,7 @@ gem 'rails', '3.1.3'
 group :development do
   gem 'sqlite3', '1.3.5'
   gem 'rspec-rails', '2.8.1'
+  gem 'annotate', '~> 2.4.1.beta'
 end
 
 group :production do
@@ -26,6 +27,7 @@ gem 'jquery-rails'
 
 # To use ActiveModel has_secure_password
 # gem 'bcrypt-ruby', '~> 3.0.0'
+gem 'bcrypt-ruby', '3.0.1'
 
 # Use unicorn as the web server
 # gem 'unicorn'
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,5 @@
 module ApplicationHelper
-# Returns the full title on a per-page basis.
+  # Returns the full title on a per-page basis.
   def full_title(page_title)
     base_title = "iService Application"
     if page_title.empty?
@@ -7,5 +7,9 @@ module ApplicationHelper
     else
       "#{base_title} | #{page_title}"
     end
+  end
+  # get logo
+  def logo(logo_name)
+    image_tag(logo_name, alt: "iService App", class: "round", width: "300", height: "55") 
   end
 end
